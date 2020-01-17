cab5abd3798593e930e07ff67d504520b34e9073
Yukihira Nanako <yuki@yuki.kr>
2020-01-17 17:48:54 +0900

    Init project

---
+++ STKit/STKit.h
@@ -0,0 +1,19 @@
+//
+//  STKit.h
+//  STKit
+//
+//  Created by Insu Byeon on 2020/01/17.
+//  Copyright © 2020 Insu Byeon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for STKit.
+FOUNDATION_EXPORT double STKitVersionNumber;
+
+//! Project version string for STKit.
+FOUNDATION_EXPORT const unsigned char STKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STKit/PublicHeader.h>
+
+
---
+++ STKit/STKitApp.swift
@@ -0,0 +1,21 @@
+//
+//  STKitApp.swift
+//  STKit
+//
+//  Created by Insu Byeon on 2020/01/17.
+//  Copyright © 2020 Insu Byeon. All rights reserved.
+//
+
+import Foundation
+
+public struct STKit {
+    
+    public static func show() {
+        
+    }
+    
+    public static func hide() {
+        
+    }
+    
+}
